2347588315b2c50bf39bca3084c0c4d19a04850a
Mohamed Sadek <mohamedfawzydes@gmail.com>
2025-03-19 17:14:56 +0100

    Add loops

--- Gruntfile.js
+++ Gruntfile.js
@@ -201,6 +201,8 @@ module.exports = function(grunt) {
                     "packages/node_modules/@node-red/editor-client/src/js/ui/subflow.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/group.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/userSettings.js",
+                    "packages/node_modules/@node-red/editor-client/src/js/ui/openai-service.js",
+                    "packages/node_modules/@node-red/editor-client/src/js/ui/chatbot.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/projects/projects.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/projects/projectSettings.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/projects/projectUserSettings.js",
